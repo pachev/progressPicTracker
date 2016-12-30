3ab74f9c1d599d34efb63b478f6c9bc3cdb87a38
Pachev Joseph <pachevjoseph@gmail.com>
2016-12-30 14:23:08 -0500

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,4 @@
+import React, { AppRegistry } from 'react-native';
+import progressPicTracker from './src/progressPicTracker';
+
+AppRegistry.registerComponent('progressPicTracker', () => progressPicTracker);
